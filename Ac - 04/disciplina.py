4fa43e15909e9987a575ec3ccdc6128f79c7d030
Filipe Furtado <filipegomesfurtado@gmail.com>
2018-09-11 19:39:10 -0300

    Update disciplina.py

--- Ac - 04/disciplina.py
+++ Ac - 04/disciplina.py
@@ -5,3 +5,40 @@ class disciplina:
         self.percentualPratico = percentualPratico
         self.percentualTeorico = percentualTeorico
         self.professor = professor
+
+        auxDisciplinas = []
+        nomeDB = {}
+        cargaHorariaDB = {}
+        percPraticoDB = {}
+        percTeoricoDB = {}
+        professorDB = {}
+
+        with open("disciplinas.txt") as f:
+            aux = f.read().splitlines()
+            f.close()
+
+        for item in aux:
+            auxDisciplinas = item.split(";")
+            nomeDB[auxDisciplinas[0]] = auxDisciplinas[0]
+            cargaHorariaDB[auxDisciplinas[0]] = auxDisciplinas[1]
+            percPraticoDB[auxDisciplinas[0]] = auxDisciplinas[2]
+            percTeoricoDB[auxDisciplinas[0]] = auxDisciplinas[3]
+            professorDB[auxDisciplinas[0]] = auxDisciplinas[4]
+
+        if self.nome in nomeDB:
+            print("Disciplina já cadastrada!\n")
+        else:        
+            DB = open("disciplinas.txt", "a")
+
+            nomeDB[self.nome] = self.nome
+            cargaHorariaDB[self.nome] = self.cargaHoraria
+            percPraticoDB[self.nome] = self.percentualPratico
+            percTeoricoDB[self.nome] = self.percentualTeorico
+            professorDB[self.nome] = self.professor
+
+            for j in nomeDB:
+                DB.write(nomeDB[j] + ';' + cargaHorariaDB[j] + ';' + percPraticoDB[j] + ';' + percTeoricoDB[j] + ';' + professorDB[j] + '\n')
+
+            DB.close()
+
+            print("Disciplina cadastrada com sucesso!\n")
