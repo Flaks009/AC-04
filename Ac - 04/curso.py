66e8e094644f43978be97dba42de229bc0e30278
Bruno Flaks <42192055+Flaks009@users.noreply.github.com>
2018-09-12 01:07:54 -0300

    Update curso.py

--- Ac - 04/curso.py
+++ Ac - 04/curso.py
@@ -1,53 +1,74 @@
 class curso:
-    listaCursos = []
-    def __init__(self, nome, periodo, cod_curso = 0, disciplinas = [], status = False):
-        self.nome = nome
+    listaCurso = {}
+    cursosCadastrados = []
+
+    def __init__(self, nome, periodo, disciplinas = [], status = "Desativado"):
+
+        self.nome = nome + "*"
+
         self.periodo = periodo
-        self.cod_curso = len(self.listaCursos) + 1
-        self.disciplinas = {}
-        self.status = False
-        
+
+        self.disciplinas = disciplinas
+
+        self.status = "Desativado"
+
+        self.listaCurso[self.nome] = {"Nome":self.nome, "Periodo":self.periodo, "Disciplina":self.disciplinas, "Status":self.status}
 
     def cadastraCurso(self):
-            
-        self.listaCursos.append([self.nome, self.cod_curso, self.periodo, self.disciplinas])
-        self.status = True
-            
 
+        if self.listaCurso[self.nome]["Nome"] == "Desativado":
+            self.listaCurso[self.nome]["Status"] = "Ativado"
+        
+        self.cursosCadastrados.append(self.nome)
+        self.listaCurso[self.nome]["Status"] = "Ativado"
+        
         with open('cursos.txt', 'a+') as Cursos:
 
-            Cursos.write("--------------------------------------------\n")
-            Cursos.write("Nome do curso: {}\n" .format(self.nome))
-            Cursos.write("Código do curso: {}\n" .format(self.cod_curso))
-            Cursos.write("Período: {}\n" .format(self.periodo))
-            Cursos.write("Disciplinas: {}\n" .format(self.disciplinas))
-            Cursos.write("--------------------------------------------\n")
+            Cursos.write(self.nome + ";" + self.periodo + ";")
 
-                
             Cursos.close()
+
         
-    def exibeCurso(self):
 
-        cod_busca = int(input("Código do curso:"))          
+    def Cursos(self):
 
-        if cod_busca == 0:
-            print(self.listaCursos)
-        
-        else:
-            print(self.listaCursos[cod_busca])
+        with open('cursos.txt', 'r') as Cursos:
 
-    def desativaCurso(self):
-        pass
+            for item in Cursos:
+                item = item.splitlines()
+                for item in item:
+                    item2 = item.split(";")
+                    if '*' in item2:
+                        self.cursosCadastrados.append(item2)
+        Cursos.close()
+
+        return self.cursosCadastrados
+
+    def exibeCursos(self):
+
+        for curso in self.cursosCadastrados:
+            print("*"*20)
+            print("Nome: {}" .format(self.listaCurso[curso]["Nome"]))
+            print("Periodo: {}" .format(self.listaCurso[curso]["Periodo"]))
+            print("Disciplinas {}" .format(self.listaCurso[curso]["Disciplina"]))
+            print("Status {}" .format(self.listaCurso[curso]["Status"]))
+            print("*"*20)
 
 
+    def alteraStatus(self):
+        if self.listaCurso[self.nome]["Status"] == "Ativado":
+            self.listaCurso[self.nome]["Status"] = "Desativado"
 
-ADS = curso("ADS", "Manhã")
+        elif self.listaCurso[self.nome]["Status"] == "Desativado":
+            self.listaCurso[self.nome]["Status"] = "Ativado"
+
+
+ADS = curso("ADS", "Manha",["Logica", "Programacao"])
 ADS.cadastraCurso()
-Ingles = curso("Ingles", "Noite")
-Ingles.cadastraCurso()
-SI = curso("SI", "Manhã")
-SI.cadastraCurso()
-Banco = curso("Banco", "Noite")
+Banco = curso("Banco", "Noite", ["Banco de dados", "Linguagem SQL"])
 Banco.cadastraCurso()
+SI = curso("SI", "Tarde", ["Modelagem", "Gestao"])
+SI.cadastraCurso()
+Banco.alteraStatus()
+SI.exibeCursos()
 
-ADS.exibeCurso()
